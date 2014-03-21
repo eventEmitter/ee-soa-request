d2bc987cd3e0626e017e7c8652cbf1e55d32fdaa
mruefenacht <ruefe@rcky.ch>
2014-03-21 08:51:24 +0100

    Cleanup

--- index.js
+++ index.js
@@ -4,80 +4,41 @@ var http        = require('http'),
     factories   = require('./lib/factory');
 
 var log = console.log;
-
 /**
- * All streams are instances of EventEmitter
- * http://nodejs.org/api/events.html#events_class_events_eventemitter
+ * Default Headers, should be included in the response.
+ * - content-length
+ * - connection
+ * - accept-encoding
+ *
+ *
+ * Request Headers:
+ * ------------------------------------------------------
+ * - Content-length:                            -> length:int
+ * - Connection: keep-alive                     -> -
+ * - Accept-encoding: gzip, deflate             -> -
+ * - Accept: application/JSON                   -> format:string
+ * - Accept-Language: de, fr;q=0.9, en;q=0.8    -> languages:array
+ * - Content-Language: de                       -> language
+ * - Range: 0-10                                -> range:object {from, to}
+ * - Content-Type: Multipart/Form-Data          -> contentType:string
+ * - API-Version: 0.0.1                         -> version:string
+ * - Date:
+ *
+ * Preprocessed
+ * - Select: id, name, tenant.id, friend.name, friend.id
+ * - Filter: id=in(3,4)
+ * - Order: name, friends.name DESC
+ *
+ * Response Headers:
+ * ------------------------------------------------------
+ * Content-Type: Application/JSON
+ * Accept-Ranges: resources
+ * Content-Range: 0-9/6000
+ * Content-Language: en
+ * Content-Length:
+ * Location: /user/1
+ * Date:
  */
-
-function getHeaders(prototype, request)
-{
-
-    var headers = {};
-    for(var name in prototype){
-        headers[name.toLowerCase()] = request.headers[name.toLowerCase()] || prototype[name];
-    }
-    return headers;
-}
-
-function getRequestCollection(request)
-{
-    var headers = getHeaders(
-        {   'accept':               'json'                  // -> accept*
-            , 'api-version':        '1'                     // -> version*
-            , 'accept-language':    'en_us'                 // -> language
-            , 'range':              {start:0, stop:10}      // -> range {start, stop}
-            , 'select':             null                    // -> fields
-            , 'order':              null                    // -> order
-            , 'filter':             []                      // -> filters
-        }, request);
-    log(headers);
-}
-
-function getRequest(request)
-{
-    var headers = getHeaders(
-        {   'accept':               'json'              // -> accept*
-            , 'api-version':        '1'                 // -> version*
-            , 'accept-language':    'en_us'             // -> language
-            , 'select':             null                  // -> fields
-        }, request);
-
-    var query       = url.parse(request.url, true),
-        queryArgs   = req.splitQueryPath(query.path);
-    // if this is longer than 1 we need subrequests
-    log(queryArgs);
-    /**
-     * Create a get request for every
-     */
-}
-
-function postRequest(request)
-{
-    var headers = getHeaders(
-        {   'accept':               'json'              // -> accept*
-            , 'content-language':   'en_us'             // -> languages*
-            , 'api-version':        '1'                 // -> version*
-            , 'content-type':       'json'              // -> format*
-            , 'select':             null                // -> fields
-        }, request);
-    log(headers);
-}
-
-function putRequest(request)
-{
-
-}
-
-function deleteRequest(request){
-    var headers = getHeaders(
-        {   'accept':               'json'          // -> accept*
-            , 'api-version':        '1'             // -> version*
-            , 'select':             null            // -> fields*
-            , 'content-language':   'en_us'         // -> language
-        }, request);
-    log(headers);
-}
 
 http.createServer(function(request, response){
     var factory = new factories.HTTPRequestFactory();
