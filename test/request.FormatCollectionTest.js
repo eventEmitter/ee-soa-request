728c9005eb14c31103f599202b4a7396e4ff1595
mruefenacht <ruefe@rcky.ch>
2014-03-26 14:48:35 +0100

    Added formats and some tests

---
+++ test/request.FormatCollectionTest.js
@@ -0,0 +1,41 @@
+"use strict";
+var request = require('../lib/request'),
+    assert = require('assert');
+
+var FormatCollection    = request.FormatCollection,
+    Format              = request.Format;
+
+describe('FormatCollection', function(){
+
+        var formatJSON      = new Format('application', 'json'),
+            formatJPEG      = new Format('image', 'jpeg'),
+            formatImages    = new Format('image'),
+            formatPlainText = new Format('text', 'plain'),
+            formatWildCard  = new Format();
+
+
+        describe( '#isCompatibleTo', function() {
+
+            it('should detect if types are compatible or not', function(){
+                var collection = new FormatCollection().pushAll([formatJSON, formatImages]);
+                assert(collection.isCompatibleTo(formatJSON));
+            });
+
+            it('should should be compatible to subtypes (contravariance)', function(){
+                var collection = new FormatCollection().pushAll([formatJSON, formatImages]);
+                assert(collection.isCompatibleTo(formatJPEG));
+            });
+
+            it('should should be incompatible to supertypes', function(){
+                var collection = new FormatCollection().pushAll([formatJSON, formatJPEG]);
+                assert(!collection.isCompatibleTo(formatImages));
+                assert(!collection.isCompatibleTo(formatWildCard));
+            });
+
+            it('should should be incompatible to other types', function(){
+                var collection = new FormatCollection().pushAll([formatJSON, formatJPEG]);
+                assert(!collection.isCompatibleTo(formatPlainText));
+            });
+        });
+    }
+);
---
+++ test/request.FormatTest.js
@@ -0,0 +1,49 @@
+"use strict";
+var request = require('../lib/request'),
+    assert = require('assert');
+
+var Format = request.Format;
+
+describe('Format', function(){
+
+        var formatJSON      = new Format('application', 'json'),
+            formatJPEG      = new Format('image', 'jpeg'),
+            formatImages    = new Format('image'),
+            formatWildCard  = new Format();
+
+        describe( '#isWildcard', function(){
+            it('should identify if its a wildcard or not', function(){
+                assert(formatWildCard.isWildCard());
+                assert(!formatJSON.isWildCard());
+            });
+
+            it('should identify only as wildcard if main type is a wildcard!', function(){
+                assert(!formatImages.isWildCard());
+            });
+        });
+
+        describe( '#isCompatibleTo', function() {
+
+            it('should detect if types are compatible or not', function(){
+                assert(formatJSON.isCompatibleTo(formatJSON));
+                assert(!formatJSON.isCompatibleTo(formatJPEG));
+            });
+
+            it('should detect if main types are compatible', function(){
+                assert(formatJPEG.isCompatibleTo(formatImages));
+            });
+
+            it('should detect if main types are compatible', function(){
+                assert(formatJPEG.isCompatibleTo(formatImages));
+            });
+
+            it('should always be true on wildcards', function(){
+                assert(formatJPEG.isCompatibleTo(formatWildCard));
+            });
+
+            it('should not allow wildcards to be compatible with more specific types!', function(){
+                assert(!formatWildCard.isCompatibleTo(formatImages));
+            });
+        });
+    }
+);
