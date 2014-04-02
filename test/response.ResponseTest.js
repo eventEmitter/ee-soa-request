ddf388b05fd3325ddde044effe38f634ad17344e
mruefenacht <ruefe@rcky.ch>
2014-04-02 13:53:43 +0200

    Started working on response and fixed typos

---
+++ test/response.ResponseTest.js
@@ -0,0 +1,20 @@
+"use strict";
+var response = require('../lib/response'),
+    assert = require('assert');
+
+var Response = response.Response;
+
+describe('Responst', function(){
+
+        /*describe( '#isWildcard', function(){
+            it('should identify if its a wildcard or not', function(){
+                assert(formatWildCard.isWildCard());
+                assert(!formatJSON.isWildCard());
+            });
+
+            it('should identify only as wildcard if main type is a wildcard!', function(){
+                assert(!formatImages.isWildCard());
+            });
+        });*/
+    }
+);
